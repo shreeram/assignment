6ce75dadfff1ee0f399f83dc6d885abfbd60f392
aramesh <ramesh.adaikkalam@sc.com>
2017-09-05 03:53:33 +0800

    initial assignment commit

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,13 +2,69 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/view1', {
+      templateUrl: 'view1/view1.html',
+      controller: 'View1Ctrl'
+    });
+  }])
+  .controller('View1Ctrl', ['$scope', function($scope) {
+    $scope.column = 8;
+    $scope.row = 6;
 
-.controller('View1Ctrl', [function() {
+    $scope.getNumber = function(number)
+    {
+      return new Array(number);
+    };
+    $scope.clear = function(array)
+    {
+      for(var i=0;i<$scope.column;i++)
+      {
+        array[i] = [];
+        for(var j=0;j<$scope.row;j++)
+        {
+          array[i][j] =false;
+        }
+      }
+    };
+    $scope.toggleCell = function(i, j)
+    {
+      $scope.input[i][j] = !$scope.input[i][j];
+    };
 
-}]);
+    $scope.nextGeneration = function(input, M, N) {
+
+
+      for (var l = 1; l < M - 1; l++) {
+        for (var m = 1; m < N - 1; m++) {
+          var aliveNItems = 0;
+          for (var i = -1; i <= 1; i++)
+            for (var j = -1; j <= 1; j++)
+            {
+              aliveNItems += (input[l + i][m + j] ? 1 : 0);
+            }
+
+          aliveNItems -= (input[l][m] ? 1 : 0);
+
+          if ((input[l][m]) && (aliveNItems < 2))
+            $scope.output[l][m] = false;
+
+          else if ((input[l][m]) && (aliveNItems > 3))
+            $scope.output[l][m] = false;
+
+          else if ((!input[l][m]) && (aliveNItems == 3))
+            $scope.output[l][m] = true;
+
+          else
+            $scope.output[l][m] = input[l][m];
+        }
+      }
+    };
+
+
+    // initialize the input array
+    $scope.input = [];
+    $scope.output = [];
+    $scope.clear($scope.input);
+    $scope.clear($scope.output);
+  }]);
--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -6,10 +6,66 @@ describe('myApp.view1 module', function() {
 
   describe('view1 controller', function(){
 
-    it('should ....', inject(function($controller) {
+    // it('should ....', inject(function($controller) {
+    //   //spec body
+    //   var view1Ctrl = $controller('View1Ctrl');
+    //   expect(view1Ctrl).toBeDefined();
+    // }));
+
+    it('should nextGeneration', inject(function($controller, $rootScope ) {
       //spec body
-      var view1Ctrl = $controller('View1Ctrl');
-      expect(view1Ctrl).toBeDefined();
+
+      var scope = $rootScope.$new();
+      var view1Ctrl = $controller('View1Ctrl',{ $scope: scope });
+
+      var array1 = [];
+      var array2 = [];
+      scope.clear(array1);
+      scope.clear(array2);
+      array1[3][3] = true;
+      array1[3][4] = true;
+      array1[3][5] = true;
+      scope.nextGeneration(array1, 8, 6);
+
+      expect(scope.output[3][4]).toBeTruthy();
+      expect(scope.output[2][4]).toBeTruthy();
+      expect(scope.output[4][4]).toBeTruthy();
+    }));
+
+    it('should nextGeneration', inject(function($controller, $rootScope ) {
+      //spec body
+
+      var scope = $rootScope.$new();
+      var view1Ctrl = $controller('View1Ctrl',{ $scope: scope });
+
+      var array1 = [];
+      var array2 = [];
+      scope.clear(array1);
+      scope.clear(array2);
+      array1[2][2] = true;
+      array1[2][3] = true;
+      array1[3][2] = true;
+      array1[3][3] = true;
+
+      scope.nextGeneration(array1, 8, 6);
+
+      expect(scope.output[2][2]).toBeTruthy();
+      expect(scope.output[2][3]).toBeTruthy();
+      expect(scope.output[3][2]).toBeTruthy();
+      expect(scope.output[3][3]).toBeTruthy();
+    }));
+
+    it('should clear', inject(function($controller, $rootScope ) {
+      //spec body
+
+      var scope = $rootScope.$new();
+      var view1Ctrl = $controller('View1Ctrl',{ $scope: scope });
+
+      var array1 = [];
+      array1[0] = [true];
+      scope.clear(array1);
+      expect(array1[0][0]).toBeFalsy();
+
     }));
 
   });
